eb99f25bf5c09757c6a4f3e6ed574bb6abafa437
云中子·悟玄 <roc9574@sina.com>
2019-08-16 15:23:31 +0800

    Spring IoC 容器

---
+++ bean/src/main/java/com/shiyanlou/spring/customer/services/CustomerService.java
@@ -0,0 +1,14 @@
+package com.shiyanlou.spring.customer.services;
+
+public class CustomerService
+{
+    String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
