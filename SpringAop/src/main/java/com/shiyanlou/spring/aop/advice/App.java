2e3f7302096b83adf823d0d6cf0a45daef822f73
ID9574 <roc9574@sina.com>
2019-08-19 09:13:48 +0800

    Spring 框架的 AOP

--- SpringAop/src/main/java/com/shiyanlou/spring/aop/advice/App.java
+++ SpringAop/src/main/java/com/shiyanlou/spring/aop/advice/App.java
@@ -5,20 +5,43 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
+    // public static void main(String[] args) {
+    //     ApplicationContext appContext = new ClassPathXmlApplicationContext(
+    //             new String[] { "SpringAOPAdvice.xml" });
+
+    //     CustomerService cust = (CustomerService) appContext.getBean("customerService");
+
+    //     System.out.println("*************************");
+    //     cust.printName();
+    //     System.out.println("*************************");
+    //     cust.printURL();
+    //     System.out.println("*************************");
+    //     try {
+    //         cust.printThrowException();
+    //     } catch (Exception e) {
+    //     }
+    // }
+
+    //before advice
     public static void main(String[] args) {
         ApplicationContext appContext = new ClassPathXmlApplicationContext(
-                new String[] { "SpringAOPAdvice.xml" });
+                new String[] { "ApringAOPAdvice.xml" });
 
-        CustomerService cust = (CustomerService) appContext.getBean("customerService");
+        CustomerService cust = (CustomerService) appContext.getBean("customerServiceProxy");
 
+        System.out.println("使用Spring AOP 如下");
         System.out.println("*************************");
         cust.printName();
         System.out.println("*************************");
         cust.printURL();
         System.out.println("*************************");
+
         try {
             cust.printThrowException();
         } catch (Exception e) {
+
         }
+
     }
+
 }
